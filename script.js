2f0adfe6969e582247a7c39d8ee8073193c5a219
‘christopher-distasio’ <stahzz1970@gmail.com>
2023-07-29 22:31:59 -0500

    Adds image folder, demon phase images

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 
 
+// document.getElementById("test").innerHTML = "This is from the script.js file"
+
 document.getElementById("test").innerHTML = "This is from the script.js file"
 
 
+function attack (){
+
+}
